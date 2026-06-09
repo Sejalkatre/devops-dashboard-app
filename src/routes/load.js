e77b42650c794142b6a67353224d00b9d11e9c87
Sejal Katre <96768363+Sejalkatre@users.noreply.github.com>
2026-06-09 16:15:26 +0530

    Update load.js

--- src/routes/load.js
+++ src/routes/load.js
@@ -1,22 +1,17 @@
 const express = require('express');
-
 const router = express.Router();
 
-const {
-    generateLoad
-} = require('../services/cpuStress');
+const { generateLoad } = require('../services/cpuStress');
 
 router.get('/', (req, res) => {
 
-    const duration =
-        Number.parseInt(req.query.duration || "10", 10);
+    const duration = Number.parseInt(req.query.duration || "10", 10);
 
     generateLoad(duration);
 
     res.json({
         message: `Load generated for ${duration} seconds`
     });
-
 });
 
 module.exports = router;
